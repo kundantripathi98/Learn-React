171814dc03e4bba602929ef1f9d822db50cb57ba
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-03-08 23:28:02 +0530

    Talk is cheap, show me the code!

--- Episode-4/src/components/Body.jsx
+++ Episode-4/src/components/Body.jsx
@@ -2,7 +2,11 @@ import ResCard from "./Res-card";
 
 let Body = () => {
     return <>
-           
+            <div className="filter">
+                <input type="text" name="" id="searchBox" placeholder="Search Here!"/> 
+                <button>Search</button>
+                <button>Top Rated Restaurants</button>
+                <button>Least Rated Restaurants</button>
             </div> 
         <ResCard />
     </>

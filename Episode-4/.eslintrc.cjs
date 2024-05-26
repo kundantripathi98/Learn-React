b6cb80d0b2fa2d53231c2c63f906ef68ad1d5937
Kundan Tripathi <124955318+kundantripathi98@users.noreply.github.com>
2024-05-26 19:59:16 +0530

    Update .eslintrc.cjs

--- Episode-4/.eslintrc.cjs
+++ Episode-4/.eslintrc.cjs
@@ -19,3 +19,11 @@ module.exports = {
     ],
   },
 }
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
